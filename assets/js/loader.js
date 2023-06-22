4cff18ef87ea732f124e3233ec799ba43701a6e1
meeeeeeeep1 <75493196+meeeeeeeep1@users.noreply.github.com>
2023-06-22 16:07:55 -0400

    Update loader.js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -47,20 +47,6 @@ function makeCodeRun(options) {
         if (!code || !isReady || started)
             return
         setState("run");
-        // Create a new button element
-var button = document.createElement("button");
-button.innerHTML = "Click Me";
-
-// Get the first element with the class name "ui items simPanel"
-var elements = document.getElementsByClassName("ui items simPanel");
-if (elements.length > 0) {
-  var targetElement = elements[0];
-
-  // Append the button to the target element
-  targetElement.appendChild(button);
-} else {
-  console.log("No elements found with the specified class name.");
-}
         started = true;
         const runMsg = {
             type: "run",
