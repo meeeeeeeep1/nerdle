710a61f04fe4749088ef12dcf2e095936eeed04a
meeeeeeeep1 <75493196+meeeeeeeep1@users.noreply.github.com>
2023-06-22 15:52:49 -0400

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,14 @@
-/**
- * This will be loaded before starting the simulator.
- * If you wish to add custom javascript, 
- * ** make sure to add this line to pxt.json**
- * 
- *      "disableTargetTemplateFiles": true
- * 
- * otherwise MakeCode will override your changes.
- * 
- * To register a constrol simmessages, use addSimMessageHandler
- */
+// Create a new button element
+var button = document.createElement("button");
+button.innerHTML = "Click Me";
+
+// Get the first element with the class name "ui items simPanel"
+var elements = document.getElementsByClassName("ui items simPanel");
+if (elements.length > 0) {
+  var targetElement = elements[0];
+
+  // Append the button to the target element
+  targetElement.appendChild(button);
+} else {
+  console.log("No elements found with the specified class name.");
+}
